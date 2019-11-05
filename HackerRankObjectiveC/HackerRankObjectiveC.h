b10ece0c4cc0a41c9a632e374d4968620367ba7c
Reetalibrarian <j51038@gmail.com>
2019-11-05 20:41:24 +0900

    Initial Commit

---
+++ HackerRankObjectiveC/HackerRankObjectiveC.h
@@ -0,0 +1,19 @@
+//
+//  HackerRankObjectiveC.h
+//  HackerRankObjectiveC
+//
+//  Created by Yi-Ling Wu on 2019/11/05.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HackerRankObjectiveC.
+FOUNDATION_EXPORT double HackerRankObjectiveCVersionNumber;
+
+//! Project version string for HackerRankObjectiveC.
+FOUNDATION_EXPORT const unsigned char HackerRankObjectiveCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HackerRankObjectiveC/PublicHeader.h>
+
+
