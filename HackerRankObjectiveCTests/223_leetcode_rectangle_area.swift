3bb883dd940a7218763882c64fe74a3e3b35f3f7
YILING WU <ylwulibrarian@gmail.com>
2019-11-10 16:37:25 +0900

    [Leetcode] 223 completed
    * 20 ms, faster than 94.44%
    * 20.9 MB, less than 100.00%

--- HackerRankObjectiveCTests/223_leetcode_rectangle_area.swift
+++ HackerRankObjectiveCTests/223_leetcode_rectangle_area.swift
@@ -19,7 +19,10 @@ class _23_leetcode_rectangle_area: XCTestCase {
     func testComputeArea() {
         let solution = Solution()
         
-        var result = solution.computeArea(-2, -2, 2, 2, 3, 3, 4, 4)
+        var result = solution.computeArea(-3, 0, 3, 4, 0, -1, 9, 2)
+        XCTAssertTrue(result == 45)
+        
+        result = solution.computeArea(-2, -2, 2, 2, 3, 3, 4, 4)
         XCTAssertTrue(result == 17)
         
         result = solution.computeArea(-5, -5, 0, 3, -3, -3, 3, 3)
@@ -36,12 +39,14 @@ class _23_leetcode_rectangle_area: XCTestCase {
 class Solution {
     func computeArea(_ A: Int, _ B: Int, _ C: Int, _ D: Int, _ E: Int, _ F: Int, _ G: Int, _ H: Int) -> Int {
         var intercept: Int = 0
-        if A > E && A > G || C < E && C < G || B > H && B > F || F > D && F > B {
+        if A > G || C < E || B > H || F > D {
             // ignore
         } else {
-            let sortedRow = [A, C, G, E].sorted()
-            let sortedCol = [B, D, H, F].sorted()
-            intercept = (sortedRow[2] - sortedRow[1]) * (sortedCol[2] - sortedCol[1])
+            let x1 = max(A, E)
+            let x2 = min(C, G)
+            let y1 = max(B, F)
+            let y2 = min(D, H)
+            intercept = (x2 - x1) * (y2 - y1);
         }
         return (C-A)*(D-B) + (G-E)*(H-F) - intercept
     }
