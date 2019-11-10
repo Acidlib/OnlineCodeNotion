5a9ff8e633f3d1fad54d5c64c3fa54df589ae4c2
YILING WU <ylwulibrarian@gmail.com>
2019-11-10 17:23:19 +0900

    [Leetcode] 223 rectangle
    * create files

---
+++ HackerRankObjectiveCTests/Leetcode_60_PermutationSequence.swift
@@ -0,0 +1,28 @@
+//
+//  Leetcode_60_PermutationSequence.swift
+//  HackerRankObjectiveCTests
+//
+//  Created by Yi-Ling Wu on 2019/11/10.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import XCTest
+
+class Leetcode_60_PermutationSequence: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+    
+    func testComputeArea() {
+        
+    }
+}
+
+class Solution {
+    func getPermutation(_ n: Int, _ k: Int) -> String {
+        
+    }
+}
