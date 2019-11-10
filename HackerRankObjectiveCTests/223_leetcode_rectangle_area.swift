f528f05e380c6b33848c2069a1a59113a0a1e340
YILING WU <ylwulibrarian@gmail.com>
2019-11-10 15:23:34 +0900

    [Leetcode] 223 assert

--- HackerRankObjectiveCTests/223_leetcode_rectangle_area.swift
+++ HackerRankObjectiveCTests/223_leetcode_rectangle_area.swift
@@ -15,5 +15,17 @@ class _23_leetcode_rectangle_area: XCTestCase {
 
     override func tearDown() {
     }
+    
+    func testComputeArea() {
+        let solution = Solution()
+        let result = solution.computeArea(-3, 0, 3, 4, 0, -1, 9, 2)
+        XCTAssertTrue(result == 45)
+    }
 
+}
+
+class Solution {
+    func computeArea(_ A: Int, _ B: Int, _ C: Int, _ D: Int, _ E: Int, _ F: Int, _ G: Int, _ H: Int) -> Int {
+        
+    }
 }
