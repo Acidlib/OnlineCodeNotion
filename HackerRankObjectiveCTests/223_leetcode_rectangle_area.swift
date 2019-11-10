ccdfca5a1c646a1f581469441f7abcfc832d4d24
YILING WU <ylwulibrarian@gmail.com>
2019-11-10 15:20:07 +0900

    [Leetcode] 223 rectangle
    * create file

---
+++ HackerRankObjectiveCTests/223_leetcode_rectangle_area.swift
@@ -0,0 +1,33 @@
+//
+//  223_leetcode_rectangle_area.swift
+//  HackerRankObjectiveCTests
+//
+//  Created by Yi-Ling Wu on 2019/11/10.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//  https://leetcode.com/problems/rectangle-area/
+
+import XCTest
+
+class _23_leetcode_rectangle_area: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
