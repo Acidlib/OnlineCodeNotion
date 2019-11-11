52ddd511032c06cdbe2ae319de522e6d5caec4a9
YILING WU <ylwulibrarian@gmail.com>
2019-11-11 10:34:18 +0900

    [Codility] R tests
    * create files

---
+++ HackerRankObjectiveCTests/Codility_R_test1.swift
@@ -0,0 +1,23 @@
+//
+//  Codility_R_test1.swift
+//  HackerRankObjectiveCTests
+//
+//  Created by Yi-Ling Wu on 2019/11/11.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import XCTest
+
+class Codility_R_test1: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+    
+    func testCases() {
+        
+    }
+
+}
