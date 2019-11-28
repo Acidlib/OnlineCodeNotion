d131a8c3250d956f12b4066f593c35477485d58c
YILING WU <ylwulibrarian@gmail.com>
2019-11-28 20:17:13 +0900

    [Leetcode] 912 sortAnArray: add quickSort
    // Runtime: 1432 ms, faster than 5.26% of Swift online submissions for Sort an Array.
    // Memory Usage: 25.2 MB, less than 20.00% of Swift online submissions for Sort an Array.

--- Leetcode/Leetcode_912_SortAnArray.swift
+++ Leetcode/Leetcode_912_SortAnArray.swift
@@ -16,10 +16,39 @@ class Leetcode_912_SortAnArray: XCTestCase {
     override func tearDown() {
     }
     
-    func testSortArray() {
-        var solution = SolutionHeapSort()
-        var array = [5,2,3,1]
+    func testQuickSort() {
+        let solutionQuick = SolutionQuickSort()
+        var array = [1,2,5,4,6,7,9,8,3]
+        var result = solutionQuick.sortArray(array)
+        XCTAssertTrue(result[0] == 1)
+        XCTAssertTrue(result[1] == 2)
+        XCTAssertTrue(result[2] == 3)
+        XCTAssertTrue(result[8] == 9)
+        
+    }
+    
+    func testMergeSort() {
+        var solution2 = SolutionMergeSort()
+        let array = [5,2,3,1,4,6,9,8,7]
+        let result = solution2.sortArray(array)
+        XCTAssertTrue(result[0] == 1)
+        XCTAssertTrue(result[1] == 2)
+        XCTAssertTrue(result[2] == 3)
+        XCTAssertTrue(result[8] == 9)
+    }
+    
+    // ETL
+    func testHeapSort() {
+        let solution = SolutionHeapSort()
+        var array = [1,2,5,4,6,7,9,8,3]
         var result = solution.sortArray(array)
+        XCTAssertTrue(result[0] == 1)
+        XCTAssertTrue(result[1] == 2)
+        XCTAssertTrue(result[2] == 3)
+        XCTAssertTrue(result[8] == 9)
+        
+        array = [5,2,3,1]
+        result = solution.sortArray(array)
         XCTAssertTrue(result[0] == 1)
         XCTAssertTrue(result[1] == 2)
         XCTAssertTrue(result[2] == 3)
@@ -33,23 +62,69 @@ class Leetcode_912_SortAnArray: XCTestCase {
         XCTAssertTrue(result[3] == 1)
         XCTAssertTrue(result[4] == 2)
         XCTAssertTrue(result[5] == 5)
-        
-        // merge sort
-        var solution2 = SolutionMergeSort()
-        array = [5,2,3,1,4,6,9,8,7]
-        result = solution2.sortArray(array)
-        XCTAssertTrue(result[0] == 1)
-        XCTAssertTrue(result[1] == 2)
-        XCTAssertTrue(result[2] == 3)
-        XCTAssertTrue(result[8] == 9)
     }
 
 }
 
+// Runtime: 1432 ms, faster than 5.26% of Swift online submissions for Sort an Array.
+// Memory Usage: 25.2 MB, less than 20.00% of Swift online submissions for Sort an Array.
+
+private class SolutionMergeSort {
+    func sortArray(_ nums: [Int]) -> [Int] {
+        if nums.count < 2 {
+            return nums
+        }
+
+        var arrays = [[Int]]()
+        for num in nums {
+            arrays.append([num])
+        }
+
+        while arrays.count != 1 {
+            let temp = merge(arr1: arrays[0], arr2: arrays[1])
+            arrays.append(temp)
+            arrays.removeFirst()
+            arrays.removeFirst()
+        }
+
+        return arrays[0]
+    }
+    
+    private func merge(arr1: [Int], arr2: [Int]) -> [Int] {
+        var result = [Int]()
+        var arr1 = arr1
+        var arr2 = arr2
+
+        while !arr1.isEmpty && !arr2.isEmpty {
+            if arr1[0] == arr2[0] {
+                result.append(arr1.removeFirst())
+                result.append(arr2.removeFirst())
+            } else if arr1[0] < arr2[0] {
+                result.append(arr1.removeFirst())
+            } else {
+                result.append(arr2.removeFirst())
+            }
+        }
+
+        while !arr1.isEmpty {
+            result.append(arr1.removeFirst())
+        }
+
+        while !arr2.isEmpty {
+            result.append(arr2.removeFirst())
+        }
+
+        return result
+    }
+}
+
 // heapsort: Time Limit Exceeded (maybe heapfiy takes too long)
+// Although time complexity is the same, the constant factors are not.
+// Generally merge sort will be significantly faster on a typical system with a 4 or greater way cache, since merge sort will perform sequential reads from two runs and sequential writes to a single merged run
 private class SolutionHeapSort {
     func sortArray(_ nums: [Int]) -> [Int] {
         var array = nums
+        
         heapify(&array, nums.count - 1)
         
         var endIndex = nums.count - 1
@@ -113,51 +188,48 @@ private class SolutionHeapSort {
     }
 }
 
-private class SolutionMergeSort {
+
+//Runtime: 172 ms, faster than 80.45% of Swift online submissions for Sort an Array.
+//Memory Usage: 23.8 MB, less than 20.00% of Swift online submissions for Sort an Array.
+private class SolutionQuickSort {
     func sortArray(_ nums: [Int]) -> [Int] {
-        if nums.count < 2 {
-            return nums
-        }
-
-        var arrays = [[Int]]()
-        for num in nums {
-            arrays.append([num])
-        }
-
-        while arrays.count != 1 {
-            let temp = merge(arr1: arrays[0], arr2: arrays[1])
-            arrays.append(temp)
-            arrays.removeFirst()
-            arrays.removeFirst()
-        }
-
-        return arrays[0]
+        var nums = nums
+        let result = quickSort(&nums, 0, nums.count - 1)
+        return result
     }
     
-    private func merge(arr1: [Int], arr2: [Int]) -> [Int] {
-        var result = [Int]()
-        var arr1 = arr1
-        var arr2 = arr2
-
-        while !arr1.isEmpty && !arr2.isEmpty {
-            if arr1[0] == arr2[0] {
-                result.append(arr1.removeFirst())
-                result.append(arr2.removeFirst())
-            } else if arr1[0] < arr2[0] {
-                result.append(arr1.removeFirst())
-            } else {
-                result.append(arr2.removeFirst())
+    func swap(_ array: inout [Int], _ index1: Int, _ index2: Int) {
+        let tempValue = array[index1]
+        array[index1] = array[index2]
+        array[index2] = tempValue
+    }
+    
+    func quickSort(_ listToSort: inout [Int], _ low: Int, _ high: Int) -> [Int] {
+        if low >= high { return listToSort }
+        let pivot = partition(&listToSort, low, high)
+        quickSort(&listToSort, low, pivot - 1)
+        quickSort(&listToSort, pivot + 1, high)
+        print("")
+        return listToSort
+    }
+    
+    func partition(_ listToSort: inout [Int], _ low: Int, _ high: Int) -> Int {
+        //var listToSort = listToSort
+        let pivot = listToSort[low]
+        var l = low
+        var h = high
+        while l < h {
+            while listToSort[l] <= pivot && l < h {
+                l += 1
+            }
+            while listToSort[h] > pivot {
+                h -= 1
+            }
+            if l < h {
+                swap(&listToSort, l, h)
             }
         }
-
-        while !arr1.isEmpty {
-            result.append(arr1.removeFirst())
-        }
-
-        while !arr2.isEmpty {
-            result.append(arr2.removeFirst())
-        }
-
-        return result
+        swap(&listToSort, low, h)
+        return h
     }
 }
