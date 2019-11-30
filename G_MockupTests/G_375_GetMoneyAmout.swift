1a4c43971a73081e0d4fb3e6c53a55bcd8aec487
YILING WU <ylwulibrarian@gmail.com>
2019-11-30 15:49:16 +0900

    [Leetcode] 375. GetMoneyAmount (Boost-> 43.75%)

--- G_MockupTests/G_375_GetMoneyAmout.swift
+++ G_MockupTests/G_375_GetMoneyAmout.swift
@@ -19,10 +19,11 @@ class G_375_GetMoneyAmout: XCTestCase {
 
     func testMoneyAmount() {
         let solution = Solution()
-        let result = solution.getMoneyAmount(4)
-        XCTAssertTrue(result == 4)
+        XCTAssertTrue(solution.getMoneyAmount(4) == 4)
+        
+        let soutionBoost = SolutionBoost()
+        XCTAssertTrue(soutionBoost.getMoneyAmount(4) == 4)
     }
-
 }
 
 //Runtime: 176 ms, faster than 18.75% of Swift online submissions for Guess Number Higher or Lower II.
@@ -30,10 +31,14 @@ class G_375_GetMoneyAmout: XCTestCase {
 private class Solution {
     func getMoneyAmount(_ n: Int) -> Int {
         var payment: [[Int]] = []
+        if n < 2 {
+            return 0
+        }
+        let start = Date()
         for i in 0..<n {
             payment.append([Int](repeating: 0, count: i+1))
-
         }
+        print("timeInterval: \(start.timeIntervalSince1970 - Date().timeIntervalSince1970)")
         
         for i in 0..<n {
             for j in stride(from: i, to: -1, by: -1) {
@@ -56,6 +61,70 @@ private class Solution {
                 }
             }
         }
+        print(payment)
         return payment[n-1][0]
     }
 }
+
+//Runtime: 92 ms, faster than 43.75% of Swift online submissions for Guess Number Higher or Lower II.
+//Memory Usage: 20.8 MB, less than 100.00% of Swift online submissions for Guess Number Higher or Lower II.
+private class SolutionBoost {
+    func getMoneyAmount(_ n: Int) -> Int {
+        if n < 2 {
+            return 0
+        }
+        var dp: [[Int]] = []
+        for i in 0...n {
+            dp.append([])
+            for j in 0...n {
+                dp[i].append(0)
+            }
+        }
+        
+        for i in 0...n {
+            dp[i][i] = 0
+        }
+        for offset in 1..<n {
+            for i in 1...n-offset {
+                let j = i + offset
+                var v = Int.max
+                for k in i...j {
+                    let left = dp[i][k-1]
+                    let right = k < j ? dp[k+1][j] : 0
+                    v = min(v, max(left, right) + k)
+                }
+                dp[i][j] = v
+            }
+        }
+        return dp[1][n]
+    }
+}
+
+//------ i = 1,  j = 2
+//left:1, 0  right: 2, 2
+//left:1, 1  right: 0
+//------ 2 3
+//left:2, 1  right: 3, 3
+//left:2, 2  right: 0
+//------ 3 4
+//left:3, 2  right: 4, 4
+//left:3, 3  right: 0
+//------ 1 3
+//left:1, 0  right: 2, 3
+//left:1, 1  right: 3, 3
+//left:1, 2  right: 0
+//------ 2 4
+//left:2, 1  right: 3, 4
+//left:2, 2  right: 4, 4
+//left:2, 3  right: 0
+//------ 1 4
+//left:1, 0  right: 2, 4
+//left:1, 1  right: 3, 4
+//left:1, 2  right: 4, 4
+//left:1, 3  right: 0
+//
+//[[0, 0, 0, 0, 0],
+//[0, 0, 1, 2, 4],
+//[0, 0, 0, 2, 3],
+//[0, 0, 0, 0, 3],
+//[0, 0, 0, 0, 0]]"
